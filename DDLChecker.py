c6b0c692052ac5d7d1788edfb8d35c329d938347
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-02-21 18:37:34 +0530

    Show error if no urls are present during checkall

--- DDLChecker.py
+++ DDLChecker.py
@@ -33,6 +33,9 @@ class PSADDLChecker:
 
     def checkall(self):
         self.geturls()
+        if urls[0] == "" and urls[1] == "":
+            messagebox.showerror("No URLS","No URLs present")
+            return
         open_all_response= messagebox.askyesno("Are You Sure",f"This may open {len(urls)-1} tabs. Are you sure you want to proceed?")
         if open_all_response:
             for url in urls:
