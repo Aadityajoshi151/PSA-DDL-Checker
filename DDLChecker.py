be79cc6da6785bc4b7ed6072230c691f002b8af3
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-02-22 18:20:45 +0530

    Create buttons dynamically

--- DDLChecker.py
+++ DDLChecker.py
@@ -65,6 +65,7 @@ class PSADDLChecker:
         self.urlbox.delete(1.0,END)
 
     def __init__(self,app):
+        Buttons = ['Uptobox','Mega.nz','Send.cm','Katfile','Clicknupload','Megaload','Zippyshare','Anonfiles','Bayfiles','DDownload','Megaup','Dropapk']
         self.urlbox = scrolledtext.ScrolledText(app,width=90,height=24,undo=True)  
         self.urlbox.grid(row=0,column=0,padx=10,pady=2)
 
@@ -74,41 +75,9 @@ class PSADDLChecker:
         self.horizontalbtnsframe = Frame(app)
         self.horizontalbtnsframe.grid(row=1,column=0)
 
-        self.uptoboxbtn = ttk.Button(self.verticalbtnsframe,text="Uptobox Check",command=lambda: self.checkspecefic("uptobox"))
-        self.uptoboxbtn.grid(row=0,column=0,padx=5,pady=5)
-
-        self.megabtn = ttk.Button(self.verticalbtnsframe,text="Mega Check",command=lambda: self.checkspecefic("mega.nz"))
-        self.megabtn.grid(row=1,column=0,padx=5,pady=5)
-
-        self.sendcmbtn = ttk.Button(self.verticalbtnsframe,text="Send.cm Check",command=lambda: self.checkspecefic("send.cm"))
-        self.sendcmbtn.grid(row=2,column=0,padx=5,pady=5)
-
-        self.katfilebtn = ttk.Button(self.verticalbtnsframe,text="Katfile Check",command=lambda: self.checkspecefic("katfile"))
-        self.katfilebtn.grid(row=3,column=0,padx=5,pady=5)
-
-        self.clicknuploadbtn = ttk.Button(self.verticalbtnsframe,text="Clicknupload Check",command=lambda: self.checkspecefic("clicknupload"))
-        self.clicknuploadbtn.grid(row=4,column=0,padx=5)
-
-        self.megaloadbtn = ttk.Button(self.verticalbtnsframe,text="Megaload Check",command=lambda: self.checkspecefic("megaload"))
-        self.megaloadbtn.grid(row=5,column=0,padx=5,pady=5)
-
-        self.zippysharebtn = ttk.Button(self.verticalbtnsframe,text="Zippyshare Check",command=lambda: self.checkspecefic("zippyshare"))
-        self.zippysharebtn.grid(row=6,column=0,padx=5,pady=5)
-
-        self.anonfilesbtn = ttk.Button(self.verticalbtnsframe,text="Anonfiles Check",command=lambda: self.checkspecefic("anonfiles"))
-        self.anonfilesbtn.grid(row=7,column=0,padx=5,pady=5)
-
-        self.bayfilesbtn = ttk.Button(self.verticalbtnsframe,text="Bayfiles Check",command=lambda: self.checkspecefic("bayfiles"))
-        self.bayfilesbtn.grid(row=8,column=0,padx=5,pady=5)
-
-        self.ddownloadbtn = ttk.Button(self.verticalbtnsframe,text="DDownload Check",command=lambda: self.checkspecefic("ddownload"))
-        self.ddownloadbtn.grid(row=9,column=0,padx=5,pady=5)
-
-        self.megaupbtn = ttk.Button(self.verticalbtnsframe,text="Megaup Check",command=lambda: self.checkspecefic("megaup"))
-        self.megaupbtn.grid(row=10,column=0,padx=5,pady=5)
-
-        self.dropapkbtn = ttk.Button(self.verticalbtnsframe,text="Dropapk Check",command=lambda: self.checkspecefic("dropapk"))
-        self.dropapkbtn.grid(row=11,column=0,padx=5,pady=5)
+        for i in range(len(Buttons)):
+            self.b = ttk.Button(self.verticalbtnsframe, text=f'{Buttons[i]} Check', command=lambda i=i: self.checkspecefic(Buttons[i].lower()))
+            self.b.grid(row=i, column=0,padx=5,pady=5)
 
         self.clearbtn = ttk.Button(self.horizontalbtnsframe,text="Clear",command=self.clearURLbox)
         self.clearbtn.grid(row=0,column=0,padx=5,pady=5)
