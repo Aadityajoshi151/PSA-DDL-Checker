5de869d1ad6ddac9a0485b9f9d65585ed67c79d5
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-05-21 16:15:46 +0530

    styles and basic logic added

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.getElementById("urlbox").focus();
+
+document.getElementById("open_all_btn").addEventListener("click", function(){
+    alert("Are you sure you want to open all these links?")
+})
