bffdbc5bf231c994e2d1df6a27a016ee70325710
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-05-21 16:36:11 +0530

    separator btn working

--- src/index.js
+++ src/index.js
@@ -9,3 +9,8 @@ document.getElementById("clear_btn").addEventListener("click", function(){
     urlbox.value='';
     urlbox.focus();
 })
+
+document.getElementById("separator_btn").addEventListener("click", function(){
+    urlbox.value+="\n*******************************************************\n"
+    urlbox.focus();
+})
