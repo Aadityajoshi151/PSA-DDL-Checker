a6388b5721ad500faf3e24fc09d6e72de89c2658
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-05-22 12:38:45 +0530

    getlinks function added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var urlbox = document.getElementById("urlbox");
 urlbox.focus();
 
 document.getElementById("open_all_btn").addEventListener("click", function(){
-    alert("Are you sure you want to open all these links?")
+    getUrls();
 })
 
 document.getElementById("clear_btn").addEventListener("click", function(){
@@ -14,3 +14,22 @@ document.getElementById("separator_btn").addEventListener("click", function(){
     urlbox.value+="\n*******************************************************\n"
     urlbox.focus();
 })
+
+hoster_btns = document.getElementsByClassName("btn-warning")
+for(i=0; i<hoster_btns.length;i++)
+{
+    hoster_btns[i].addEventListener("click",function(){
+        alert(this.innerHTML);
+    })
+}
+function getUrls()
+{
+    var urls = urlbox.value.split("\n");
+    for(i=0; i<urls.length; i++)
+    {     
+        window.open(urls[i], '_wnd' + i);
+    }
+    
+
+    
+}
